7c93710cce95200b6a1b938e899bad2570d61e4c
MatteoOrlando <mat.orlando88@gmail.com>
2024-05-06 13:13:01 +0200

    Create AuthController.java

---
+++ src/main/java/MatteoOrlando/CapStone/controller/AuthController.java
@@ -0,0 +1,37 @@
+package MatteoOrlando.CapStone.controller;
+
+import MatteoOrlando.CapStone.dto.NewUserDTO;
+import MatteoOrlando.CapStone.dto.UserLoginDTO;
+import MatteoOrlando.CapStone.dto.UserLoginResponseDTO;
+import MatteoOrlando.CapStone.entities.User;
+import MatteoOrlando.CapStone.exceptions.BadRequestException;
+import MatteoOrlando.CapStone.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private UserService us;
+
+    @Autowired
+    private AuthService as;
+
+    @PostMapping("/login")
+    public UserLoginResponseDTO login(@RequestBody UserLoginDTO body) {
+        return new UserLoginResponseDTO(this.as.authenticateUsersAndGenerateToken(body));
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User saveUser(@RequestBody @Validated NewUserDTO body, BindingResult validation) {
+        if (validation.hasErrors()) {
+            throw new BadRequestException(validation.getAllErrors());
+        }
+        return us.save(body);
+    }
+}
