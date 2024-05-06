7f5cbe7470400e4bd743e84db265fa1a40717022
MatteoOrlando <mat.orlando88@gmail.com>
2024-05-06 14:54:56 +0200

    Update PlatformController.java

--- src/main/java/MatteoOrlando/CapStone/controllers/PlatformController.java
+++ src/main/java/MatteoOrlando/CapStone/controllers/PlatformController.java
@@ -1,4 +1,43 @@
 package MatteoOrlando.CapStone.controllers;
 
-public class PlatformController {
+import MatteoOrlando.CapStone.entities.Platform;
+import MatteoOrlando.CapStone.services.PlatformService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/platforms")
+public  class PlatformController {
+
+    @Autowired
+    private PlatformService platformService;
+
+    @GetMapping
+    public List<Platform> getAllPlatforms(){
+        return platformService.findAllPlatforms();
+    }
+
+    @GetMapping("/{id}")
+    public Platform getPlatformById(@PathVariable Long id) {
+        return platformService.findPlatformById(id);
+        }
+
+    @PostMapping
+    public Platform createPlatform(@RequestBody Platform platform) {
+        return platformService.savePlatform(platform);
+    }
+
+    @PutMapping("/{id}")
+    public Platform updatePlatform(@PathVariable Long id, @RequestBody Platform platform) {
+        platform.setId(id);
+        return platformService.savePlatform(platform);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePlatform(@PathVariable Long id) {
+        platformService.deletePlatform(id);
+    }
+
 }
